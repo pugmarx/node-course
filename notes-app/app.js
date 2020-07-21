854324051e4ec238316e43f8d866d2344cb8bc2f
pugmarx <naqi.rizvi@gmail.com>
2020-07-21 23:29:59 +0530

    updated msg

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,5 @@
 const fs = require('fs')
 
-fs.writeFileSync('notes.txt', 'Take us to your master!')
+//fs.writeFileSync('notes.txt', 'Take us to your master!')
+
+fs.appendFileSync('notes.txt', ' We shall rule the world!')
