fdcaee35490504b1d92c26e5910100c3f87712d9
pugmarx <naqi.rizvi@gmail.com>
2020-07-26 02:00:32 +0530

    gitignore etc

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,16 +2,11 @@ const validator = require('validator')
 const getNotes = require('./notes')
 const chalk = require('chalk');
 
-//const utils = require('./utils')
 console.log(getNotes())
 
-//console.log(utils)
 
 console.log(validator.isEmail('abc_def@gh.com'))
 console.log(validator.isURL("https://pugmarx.me"))
 
-const greenMsg = chalk.green("Success!")
+const greenMsg = chalk.bold.green("Success!")
 console.log(greenMsg)
-
-// const addFunc = require('./utils.js')
-// console.log(addFunc(3, 4))
