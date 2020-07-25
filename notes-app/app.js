c98bd79acedaec2c10bffd99edf9ee481e7df070
pugmarx <naqi.rizvi@gmail.com>
2020-07-26 01:51:29 +0530

    validator and chalk

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,15 @@
+const validator = require('validator')
 const getNotes = require('./notes')
+const chalk = require('chalk');
 
+//const utils = require('./utils')
 console.log(getNotes())
 
+//console.log(utils)
 
+console.log(validator.isEmail('abc_def@gh.com'))
+console.log(validator.isURL("https://pugmarx.me"))
 
+console.log(chalk.green("Success!"))
 // const addFunc = require('./utils.js')
 // console.log(addFunc(3, 4))
